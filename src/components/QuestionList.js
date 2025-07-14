1c6007015c6fba903749203afbc50694715f49fc
Julius Kakai <cjkakai5@gmail.com>
2025-07-14 17:43:45 +0300

    initial commit

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  // Fetch questions on load
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then(setQuestions);
+  }, []);
+
+  // DELETE handler
+  function handleDeleteQuestion(id) {
+    const updatedQuestions = questions.filter((q) => q.id !== id);
+    setQuestions(updatedQuestions);
+  }
+
+  // PATCH handler
+  function handleUpdateQuestion(updatedQuestion) {
+    const updatedQuestions = questions.map((q) =>
+      q.id === updatedQuestion.id ? updatedQuestion : q
+    );
+    setQuestions(updatedQuestions);
+  }
+
+  const questionItems = questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteQuestion={handleDeleteQuestion}
+      onUpdateQuestion={handleUpdateQuestion}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
